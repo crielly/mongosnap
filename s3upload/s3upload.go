c2da6d85e33264e41ba438baa5d630687d3efccb
Connor Rielly <connor.rielly@360pi.com>
2017-03-02 01:09:07 -0500

    implement S3 streaming CLI flags

--- s3upload/s3upload.go
+++ s3upload/s3upload.go
@@ -3,22 +3,16 @@ package s3upload
 import (
 	"compress/gzip"
 	"io"
-	"log"
 	"os"
 
+	"github.com/crielly/mongosnap/logger"
 	"github.com/rlmcpherson/s3gof3r"
 )
-
-// log.Fatal() implies os.Exit(1)
-func logerror(err error) {
-	if err != nil {
-		log.Fatalf("%s\n", err)
-	}
-}
 
 // S3upload streams compressed output to S3
 func S3upload(toarchive, s3bucket, object string) {
 	keys, err := s3gof3r.EnvKeys()
+	logger.LogError(err)
 
 	// Open bucket we want to write a file to
 	s3 := s3gof3r.New("", keys)
@@ -26,7 +20,7 @@ func S3upload(toarchive, s3bucket, object string) {
 
 	// open a PutWriter for S3 upload
 	s3writer, err := bucket.PutWriter(object, nil, nil)
-	logerror(err)
+	logger.LogError(err)
 	defer s3writer.Close()
 
 	// Open a compressed writer to handle gzip and pass it to S3 writer
@@ -35,11 +29,11 @@ func S3upload(toarchive, s3bucket, object string) {
 
 	// Open files we want archived
 	file, err := os.Open(toarchive)
-	logerror(err)
+	logger.LogError(err)
 	defer file.Close()
 
 	// Pass opened file to compression writer
 	_, err = io.Copy(zipwriter, file)
-	logerror(err)
+	logger.LogError(err)
 
 }
