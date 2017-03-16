2b4183fb5f5780ad2e777d21418977c15160330d
Connor Rielly <crielly@users.noreply.github.com>
2017-03-15 21:56:03 -0400

    Read config yaml (#20)
    * first stab at marshaling yaml
    
    * this at least reads it in but I don't think I've got the struct right yet...
    
    * godep save

---
+++ readconfig/readconfig.go
@@ -0,0 +1,26 @@
+package readconfig
+
+import (
+	"io/ioutil"
+
+	"github.com/crielly/mongosnap/logger"
+	"github.com/ghodss/yaml"
+)
+
+// ReplicaConfig describes the configuration of a MongoD process
+type ReplicaConfig struct {
+	Replica string            `json:"replica"`
+	Conf    map[string]string `json:"conf"`
+}
+
+// ReadConfig prints a test MongoD Replica Config
+func ReadConfig(configPath string) (replconf ReplicaConfig, err error) {
+
+	y, err := ioutil.ReadFile(configPath)
+	logger.LogError(err)
+
+	err = yaml.Unmarshal(y, &replconf)
+	logger.LogError(err)
+
+	return
+}
