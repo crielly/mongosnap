30d9e06ed82d47b2cf44ae07da4f55478711bd80
Connor Rielly <crielly@outlook.com>
2017-06-19 20:18:52 -0400

    purge codebuild and codepipeline

--- terraform/codebuild/remote-state.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "mongosnap-tfstate"
-    key    = "backend/codebuild.tfstate"
-    region = "us-east-1"
-  }
-}
--- terraform/codepipeline/s3-bucket.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_s3_bucket" "mongosnap" {
-  bucket = "${var.artifact-bucket}"
-
-  tags {
-    Name = "mongosnap"
-  }
-}
