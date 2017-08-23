76a5dea4ff04e97cbe0377db472244ef19f2aea6
Connor <crielly@outlook.com>
2017-08-22 21:21:23 -0400

    small change to the echo in deploy script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 binurl=$(curl https://api.github.com/repos/crielly/mongosnap/releases/latest | jq -r '.assets'[0]'.browser_download_url')
-echo "Downloading mongosnap release from $binurl"
+echo "Downloading release from $binurl"
 rm -rf /usr/local/bin/mongosnap
 
 wget $binurl -O /usr/local/bin/mongosnap
