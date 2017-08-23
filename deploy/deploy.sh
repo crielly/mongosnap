500bc39e405ef7eb7bee389af3234ac6cd7b8d03
Connor <crielly@outlook.com>
2017-08-22 20:57:56 -0400

    add appspec and deploy script

---
+++ deploy/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+binurl=$(curl https://api.github.com/repos/crielly/mongosnap/releases/latest | jq -r '.assets'[0]'.browser_download_url')
+
+rm -rf /usr/local/bin/mongosnap
+
+wget $binurl -O /usr/local/bin/mongosnap
+chmod 0755 /usr/local/bin/mongosnap
+chown root:root /usr/local/bin/mongosnap
