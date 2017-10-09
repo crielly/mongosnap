5fc467673a07aeea3c76cf557f061582002fca26
Connor Rielly <crielly@outlook.com>
2017-10-08 20:16:45 -0400

    remove all TF from the repo

--- terraform/codedeploy/s3bucket.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_s3_bucket" "mongosnap" {
-    bucket = "${var.NAMESPACE}-mongosnap-${var.REGION}"
-
-    tags {
-        Name        = "${var.NAMESPACE}-mongodump-${var.REGION}"
-        Environment = "core"
-        Terraform   = "True"
-    }
-}
--- terraform/codedeploy/terraform.tfvars
+++
@@ -1,1 +0,0 @@
-../base/terraform.tfvars
