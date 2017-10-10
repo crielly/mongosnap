e0f5f1a26395698d8c167207dccf10282f2a89a4
Connor Rielly <crielly@outlook.com>
2017-10-09 21:16:40 -0400

    employ some devilry for autodeploy

--- Makefile
+++ Makefile
@@ -41,3 +41,6 @@ else
 	v${VERSION} \
 	dist/
 endif
+
+versioncheck:
+	@echo ${VERSION}
--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,8 +1,57 @@
-#!/bin/bash
-binurl=$(curl https://api.github.com/repos/crielly/mongosnap/releases/latest | jq -r '.assets'[0]'.browser_download_url')
-echo "Downloading release from $binurl"
-rm -rf /usr/local/bin/mongosnap
+#!/usr/bin/env python
+import requests
+import os
+import boto3
+import shutil
 
-wget $binurl -O /usr/local/bin/mongosnap
-chmod 0755 /usr/local/bin/mongosnap
-chown root:root /usr/local/bin/mongosnap
+def get_region():
+    return requests.get(
+        "http://169.254.169.254/latest/dynamic/instance-identity/document/"
+    ).json()['region']
+
+def get_ssm_client(region):
+    return boto3.client('ssm', region_name=region)
+
+def get_parameter(paramname, client):
+    return client.get_parameter(
+        Name=paramname
+    )['Parameter']['Value']
+
+def get_github_release(owner, repo, tag):
+    return requests.get(
+        "https://api.github.com/repos/{}/{}/releases/tags/{}".format(
+            owner, repo, tag
+        )
+    ).json()
+
+def del_existing_file(filepath):
+    try:
+        os.remove(filepath)
+    except OSError:
+        pass
+
+def save_to_disk(url, diskpath, mode):
+    payload = requests.get(
+        url, stream=True
+    )
+
+    with open(diskpath, 'wb') as outfile:
+        shutil.copyfileobj(payload.raw, outfile)
+    del payload
+
+    os.chmod(diskpath, mode)
+
+if __name__ == '__main__':
+    awsregion = get_region()
+    ssmclient = get_ssm_client(awsregion)
+    binversion = get_parameter("mongosnapversion", ssmclient)
+    release = get_github_release("crielly", "mongosnap", binversion)
+    filepath = "/usr/local/bin/mongosnap"
+
+    del_existing_file(filepath)
+
+    save_to_disk(
+        release['assets'][0]['browser_download_url'],
+        filepath,
+        "0555"
+    )
